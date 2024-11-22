eaa9bfebd59526b5b7ed3326f73cc150a5df8f7a
Liuzhu <liuzhu@njust.edu.cn>
2024-11-22 20:03:06 +0800

    [planner] 此次修改是为了第二次push力扣题到仓库里

---
+++ 1.两数之和.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        cout << "第二次尝试push" << endl;
+    }
+};
+// @lc code=end
+
